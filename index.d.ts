9f016315bd5a776c60c88c83a5288d76c2a366ff
Martin Hribar <hribar.martin@gmail.com>
2019-01-09 19:39:00 +0100

    Expose mp3 stuff

--- index.d.ts
+++ index.d.ts
@@ -5,8 +5,15 @@ declare module 'mp3' {
     export class MP3Stream {
         constructor(bitstream: Bitstream);
     }
-    export class MP3Frame { }
-    export class MP3Synth { }
+    export class MP3Frame {
+
+        decode(stream: MP3Stream): void;
+    }
+    export class MP3Synth {
+        pcm: any;
+
+        frame(frame: MP3Frame): void;
+    }
     export class Layer1 { }
     export class Layer2 { }
     export class Layer3 { }
