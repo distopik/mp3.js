c51fa120d15854e195d2ca61cbe086edc33b0331
Martin Hribar <hribar.martin@gmail.com>
2019-01-09 10:07:04 +0100

    d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,36 +1,17 @@
 
-
-
-interface MP3FrameHeader { }
-interface MP3Stream { }
-interface MP3Frame { }
-interface MP3Synth { }
-interface Layer1 { }
-interface Layer2 { }
-interface Layer3 { }
-
-
-interface Bitstream { }
-interface AVBuffer { }
-interface BufferList { }
-interface Stream { }
-
-interface mp3 {
-    MP3FrameHeader: MP3FrameHeader
-    MP3Stream: MP3Stream;
-    MP3Frame: MP3Frame;
-    MP3Synth: MP3Synth;
-    Layer1: Layer1;
-    Layer2: Layer2;
-    Layer3: Layer3;
-
-    Bitstream: Bitstream;
-    AVBuffer: AVBuffer;
-    BufferList: BufferList;
-    Stream: Stream;
-}
-
 declare module 'mp3' {
-    var main: mp3
-    export = main
+
+    export class MP3FrameHeader { }
+    export class MP3Stream { }
+    export class MP3Frame { }
+    export class MP3Synth { }
+    export class Layer1 { }
+    export class Layer2 { }
+    export class Layer3 { }
+
+
+    export class Bitstream { }
+    export class AVBuffer { }
+    export class BufferList { }
+    export class Stream { }
 }
